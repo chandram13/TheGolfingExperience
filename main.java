9ff938cbc46ecdf6f3a6b9d8592baa74d552b0e1
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-07 10:11:32 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,53 @@
+// Marvish Chandra
+
+public class reservationTimings{
+    int SevenoClockp2;
+    int SevenoClockp3;
+    int SevenoClockp4;
+    int EightoClockp2;
+    int EightoClockp3;
+    int EightoClockp4;
+    int NineoClockp2;
+    int NineoClockp3;
+    int NineoClockp4;
+    int TenoClockp2;
+    int TenoClockp3;
+    int TenoClockp4;
+    int ElevenoClockp2;
+    int ElevenoClockp3;
+    int ElevenoClockp4;
+    int TwelveoClockp2;
+    int TwelveoClockp3;
+    int TwelveoClockp4;
+    int OneoClockp2;
+    int OneoClockp3;
+    int OneoClockp4;
+
+    static void reservationPricing(String[] args){
+        reservationgTimings myReservationTiming = new ReservationTiming;
+        myReservationTiming.SevenoClockp2 = 485;
+        myReservationTiming.SevenoClockp3 = 485;
+        myReservationTiming.SevenoClockp4 = 599;
+        myReservationTiming.EightoClockp2 = 515;
+        myReservationTiming.EightoClockp3 = 599;
+        myReservationTiming.EightoClockp4 = 599;
+        myReservationTiming.NineoClockp2 = 515;
+        myReservationTiming.NineoClockp3 = 515;
+        myReservationTiming.NineoClockp4 = 599;
+        myReservationTiming.TenoClockp2 = 515;
+        myReservationTiming.TenoClockp3 = 515;
+        myReservationTiming.TenoClockp4 = 599;
+        myReservationTiming.ElevenoClockp2 = 485;
+        myReservationTiming.ElevenoClockp3 = 485;
+        myReservationTiming.ElevenoClockp4 = 599;
+        myReservationTiming.TwelveoClockp2 = 465;
+        myReservationTiming.TwelveoClockp3 = 465;
+        myReservationTiming.TwelveoClockp4 = 599;
+        myReservationTiming.OneoClockp2 = 485;
+        myReservationTiming.OneoClockp3 = 485;
+        myReservationTiming.OneoClockp4 = 599;
+
+    }
+
+
+}
